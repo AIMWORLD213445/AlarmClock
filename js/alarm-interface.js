a5a9d09c1e813e9cf56646f7b86d7dd762b4cc39
Brian Dutz and Yusuf Qedan <brian@>
2016-10-11 14:22:01 -0700

    add reset button hide form when alarm is set

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,30 +1,48 @@
-// var Weather = require('./../js/weather.js').weatherModule;
-//
-// var displayHumidity = function(city, humidityData) {
-//   $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
-// }
-
 $(document).ready(function() {
-  $('#alarm-clock').submit(function(event) {
+  var timeOut = 0;
+  var alarmIsSounding = false;
+
+  window.setInterval(function(){
+    $('#current-time').text(moment().format("HH:mm:ss"));
+    if(alarmIsSounding){
+      timeOut++;
+    }
+    if(timeOut > 10){
+      $('#alarm-countdown').text("");
+      $("#time-form").show();
+      $('#alarm-message').text("");
+      timeOut = 0;
+      alarmIsSounding = false;
+    }
+  }, 1000);
+
+  $('#time-form').submit(function(event) {
     event.preventDefault();
+    $(this).hide();
+
     $('#alarm-message').text("alarm set!");
-    var hours = $('#hours').val();
-    var minutes = $('#minutes').val();
-
-
+    var time = $('#time').val();
+    var alarmMoment = moment(time,"HH:mm");
+    $('#alarm-countdown').text("Alarm set for: " + alarmMoment.format("HH:mm")).css("color","red");
 
     var id = window.setInterval(function(){
-      $('#current-time').text(moment().format("HH:mm:ss a"));
-      $('#alarm-countdown').text("Alarm set for: " + moment().hours(hours).minutes(minutes).format("HH:mm a")).css("color","red");
-      if (moment().isSame(moment().hours(hours).minutes(minutes), "minute")) {
+      if (moment().isSame(alarmMoment, "minute")) {
         $('#alarm-message').text("alarm sounding!");
+        clearInterval(id);
+        alarmIsSounding = true;
       }
     }, 1000);
 
     $('#snooze').click(function() {
-      minutesInt = parseInt(minutes);
-      minutesInt +=5;
-      minutes = minutesInt.toString();
+      alarmMoment = alarmMoment.add(5,'minutes');
+      $('#alarm-countdown').text("Alarm set for: " + alarmMoment.format("HH:mm")).css("color","red");
+    });
+    
+    $('#reset').click(function() {
+      clearInterval(id);
+      $('#alarm-countdown').text("");
+      $("#time-form").show();
+      $('#alarm-message').text("");
     });
   });
 });
